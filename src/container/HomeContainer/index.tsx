769693ef61165645106e8a90e4ce6b5bda4d07f2
Ricardo Morillo S <ricardo@dreamcode.io>
2021-08-14 12:28:08 -0500

    Rename container

---
+++ src/container/HomeContainer/index.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export interface Props {
+	navigation: any;
+	valid: boolean;
+}
+export interface State {}
+class HomeContainer extends React.Component<Props, State> {
+
+
+	render() {
+		return (
+			
+		);
+	}
+}
+export default HomeContainer;
